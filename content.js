ff29439a893ccf9381aad5b6e5806b37ff450a07
Tharun <tharungowda30115@gmail.com>
2025-05-12 23:15:02 +0530

    added js

--- content.js
+++
@@ -1,8 +0,0 @@
-const overlay = document.createElement("div");
-overlay.id = "net-speed-overlay";
-overlay.textContent = "Speed: 0 Mbps";
-document.body.appendChild(overlay);
-
-window.addEventListener("updateNetworkSpeed", (event) => {
-  overlay.textContent = `Speed: ${event.detail} Mbps`;
-});
